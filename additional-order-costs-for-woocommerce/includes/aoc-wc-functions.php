61effb592f3e6d499657ccece2b85f2f1a8c6209
theritesite <theritesites@gmail.com>
2020-07-16 16:02:06 -0600

    cleaned up files

--- additional-order-costs-for-woocommerce/includes/aoc-wc-functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-function aoc_wc_calculate_addition_costs_on_order( $somecosts ) {
-	$sum = (float) 0.00;
-	$sum = floatval( array_sum( wp_list_pluck(  $somecosts , 'cost' ) ) );
-	return $sum;
-}
-
-?>
